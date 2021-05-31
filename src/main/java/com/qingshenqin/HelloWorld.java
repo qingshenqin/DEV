c03acd6c335999054c213f2cd47c222a037c3363
nanxing.ye <nanxing.ye@qizhidao.com>
2021-05-31 17:08:41 +0800

    1.修改1

--- src/main/java/com/qingshenqin/HelloWorld.java
+++ src/main/java/com/qingshenqin/HelloWorld.java
@@ -10,5 +10,6 @@ package com.qingshenqin;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world1");
     }
 }
