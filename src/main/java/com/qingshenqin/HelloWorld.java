537c517b715894da1fef381032e60b271b4b1cc9
qingshenqin <632929757@qq.com>
2021-05-31 17:18:40 +0800

    1.修改6

--- src/main/java/com/qingshenqin/HelloWorld.java
+++ src/main/java/com/qingshenqin/HelloWorld.java
@@ -14,5 +14,6 @@ public class HelloWorld {
         System.out.println("hello world2");
         System.out.println("hello world3");
         System.out.println("hello world4");
+        System.out.println("hello world4");
     }
 }
