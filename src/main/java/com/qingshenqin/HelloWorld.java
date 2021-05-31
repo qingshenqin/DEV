fb634290b21c9d18604e0b7df65cab4d9f94a304
nanxing.ye <nanxing.ye@qizhidao.com>
2021-05-31 16:56:40 +0800

    1.入门

---
+++ src/main/java/com/qingshenqin/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.qingshenqin;
+
+/**
+ * @author ： karl
+ * @date ：Created in 2021/5/31 16:55
+ * @description：
+ * @modified By：`
+ * @version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
